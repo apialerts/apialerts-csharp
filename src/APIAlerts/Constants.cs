6ab71a27aa3ce0daa0b58d2f9a098a410b771ccc
Jared Hall <jared@apialerts.com>
2026-03-21 23:56:46 +1100

    1.0.0

--- src/APIAlerts/Constants.cs
+++ src/APIAlerts/Constants.cs
@@ -4,5 +4,5 @@ internal static class Constants
 {
     internal const string ApiUrl = "https://api.apialerts.com/event";
     internal const string IntegrationName = "csharp";
-    internal const string Version = "2.0.0";
+    internal const string Version = "1.0.0";
 }
